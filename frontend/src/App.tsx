3a43caabe041475cd40aa2b0757cf098a13115bb
Wisley Milhomem <wisleycristiano@gmail.com>
2021-09-06 19:13:21 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/Navbar";
 
 
 function App() {
-  return (
-    <div><h1 className="text-primary"> Olṕa mundo</h1></div>
+  return (<>
+    <NavBar />
+    <div className="container">
+      <h1 className="text-primary"> Olṕa mundo</h1>
+      <DataTable />
+    </div>
+    <Footer />
+  </>
   );
 }
 
