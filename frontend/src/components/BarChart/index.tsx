116c18bd2024a518e0e882743f0be43e7cd20fd7
Wisley Milhomem <wisleycristiano@gmail.com>
2021-09-11 15:54:45 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,5 +1,58 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { TSaleSucess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type TSeriesData = {
+  name: string;
+  data: number[];
+}
+type TChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: TSeriesData[];
+}
+
 const BarChart = () => {
+  const [chartData, setChartData] = useState<TChartData>({
+    labels: {
+      categories: []
+    },
+    series: [
+      {
+        name: "",
+        data: []
+      }
+    ]
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/sucess-by-seller`)
+      .then(response => {
+        const data = response.data as TSaleSucess[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => round(100.0 * (x.deals / x.visited), 1));
+
+        setChartData({
+          labels: {
+            categories: myLabels
+          },
+          series: [
+            {
+              name: "% Sucess",
+              data: mySeries
+            }
+          ]
+        });
+        console.log("response", chartData);
+      });
+
+
+  }, []);
+
   const options = {
     plotOptions: {
       bar: {
@@ -8,21 +61,10 @@ const BarChart = () => {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]
-      }
-    ]
-  };
   return (
     <Chart
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
       type="bar"
       height="240"
     />
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -2,3 +2,9 @@ export type TSaleSum = {
   sellerName: string;
   sum: number;
 }
+
+export type TSaleSucess = {
+  sellerName: string;
+  visited: number;
+  deals: number;
+}
