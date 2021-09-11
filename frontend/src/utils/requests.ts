becbad22ea5543ed9db634d730599c041f4d2208
Wisley Milhomem <wisleycristiano@gmail.com>
2021-09-11 16:55:40 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,1 @@
-//export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://wdev-sds4.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
